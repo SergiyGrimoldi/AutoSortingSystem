2f4dfe91a1607a7b018aa0073d0e5fad6560dd6d
Sergiy Grimoldi <80816082+SergiyGrimoldi@users.noreply.github.com>
2022-01-12 11:47:44 +0100

    Updated version
    This automatically divides files  in type using extensions and folders in others called by type and category (es: Video, Audio etc)

--- main.py
+++ main.py
@@ -1,28 +1,76 @@
+ # os
 import os
-import shutil
+
+import easygui
+
+ # rich -console
+from rich.console import Console
+from rich.table import Table
+from rich.progress import track
+from rich import print
+from rich.panel import Panel
+from time import sleep
+from rich.console import Console
+
+ # easygui
+from easygui import *
+
+ # colorama
 import colorama
 from colorama import Fore, init, Back, Style
-import easygui
 init(autoreset=True)
 
+ # shutil
+import shutil
 
-title  =(Style.BRIGHT + Back.WHITE + Fore.RED + "Automatic Sorting System - By Sergiy Grimoldi ")+"\n"
-footer = "\n"+(Style.BRIGHT + Back.WHITE + Fore.RED + 'Goodbye from Automatic Sorting System by Sergiy Grimoldi')+"\n"
-
+console = Console()
 
 def clear():
+
     try:
-        os.system('clear') # Clear the screen of unix system
+        os.system("cls" if os.name=="nt"else "clear")
     except:
-        os.system('cls') # Clear the screen of windows system
+        os.system("cls")
     finally:
         None
 
-def main():
+    title = ("Folder sorting By Sergiy Grimoldi")
+    console.print(Panel(f"[green]{title}"), style="red", justify="center")
+
+def back():
+    
+    backButton = console.input(Panel(f"[red]Press X to back"))
+    
+    if backButton == "x" or back == "X":
+        menu()
+    else:
+        back()
+
+def exit_program():
+    
+    try:
+        os.system("cls" if os.name=="nt"else "clear")
+        exit()
+    
+    except:
+        exit()
+    
+    finally:
+        exit()
+
+def sort():
+
+
+    title  =(Style.BRIGHT + Back.WHITE + Fore.RED + "Automatic Sorting System - By Sergiy Grimoldi ")+"\n"
+    footer = "\n"+(Style.BRIGHT + Back.WHITE + Fore.RED + 'Goodbye from Automatic Sorting System by Sergiy Grimoldi')+"\n"
+
+
     print (title) # Print title of the program set at line 8
     ## Start of  program
 
-    directory_to_scan =  easygui.diropenbox()
+    path =  easygui.diropenbox()
+     # path = "/Users/sergiy/Downloads"
+    directory_to_scan = path
     print(f"\nDirectory scelta - Choosen directory: \n -  {directory_to_scan}\n") # stampo la direcotry scelta da scansionare (questa è quella da riordinare)
 
 
@@ -87,12 +135,150 @@ def main():
     ## And of  program
     print(footer) # Print footer of the program set at line 9
 
+    print()
+
+    print("Sorting folder by type...")
+
+     # path = easygui.diropenbox()
+    
+
+    folders_list = os.listdir(path)
+    
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Name", style="cyan", width=12)
+    table.add_column("Type")
+
+    print()
 
 
-    
-    
+     # clear()
+
+    audio_extensions = [".MP3",".WAV",".FLAC", ".AAC", ".WMA"]
+    video_extensions = [".MP4", ".WEBM", ".MPG", ".MP2", ".MPEG", ".MPE", ".MPV", ".OGG", ".M4P", ".M4V", ".AVI", ".WMV", ".MOV", ".QT", ".FLV", ".SWF", ".AVCHD"]
+    os_extension = [".ISO", ".DMG", ".PKG"]
+    compressed_extensions = [".ZIP", ".RAR", ".TAR"]
+    doc_extensions = [".DOCX", ".TXT", ".DOC", ".HTML", ".CSV", "TTF", "PDF"]
+    image_extensions = [".PNG", ".JPG", ".JPEG"]
+    apple_extensions = [".NUMBERS", ".PAGES", ".KEY"]
+    app_extensions = [".APP"]
+
+    for item in folders_list:
         
+        
+        if item.upper() in audio_extensions:
+            item_type = "Audio"
+
+        elif f".{item.upper()}" in audio_extensions:
+            item_type = "Audio"
+        
+        elif item.upper() in video_extensions:
+            item_type = "Video"
+        
+        elif f".{item.upper()}" in video_extensions:
+            item_type = "Video"
+
+        elif item.upper() in os_extension:
+            item_type = "Os"
+        
+        elif f".{item.upper()}" in os_extension:
+            item_type = "Os"
+
+        elif item.upper() in compressed_extensions:
+            item_type = "Compressed"
+
+        elif f".{item.upper()}" in compressed_extensions:
+            item_type = "Compressed"
+
+        elif item.upper() in doc_extensions:
+            item_type = "Document"
+
+        elif f".{item.upper()}" in doc_extensions:
+            item_type = "Document"
+        
+        elif item.upper() in image_extensions:
+            item_type = "Image"
+        
+        elif f".{item.upper()}" in image_extensions:
+            item_type = "Image"
+
+        elif item.upper() in apple_extensions:
+            item_type = "Apple"
+        
+        elif f".{item.upper()}" in apple_extensions:
+            item_type = "Apple"
+
+        elif item.upper() in app_extensions:
+            item_type = "App"
+        
+        elif f".{item.upper()}" in app_extensions:
+            item_type = "App"
+
+        elif item.startswith("."):
+            item_type = "System"
+        
+        
+        else:
+            item_type = "Unknown"
 
 
-clear()
-main()
+        if item_type == "System":
+             None
+        
+        elif item_type == "Unknown":
+           
+            print(f"from {original} to {target}")
+
+        else:
+            table.add_row(f"{item}",f"[green]{item_type}")
+
+            original = f"{path}/{item}"
+            target = f"{path}/{item_type}/{item}"
+     
+            print(f"from {original} to {target}")
+
+            shutil.move(original,target)
+
+    clear()
+
+    console.print(table, justify="center")
+
+    print()
+
+    back()
+
+def menu():
+   
+    clear()
+
+    console.print(Panel("[green]Menu[center]"),style="yellow", justify="center")
+    console.print(Panel("[yellow]Choose what to do:"))
+
+    selected = console.input("\n [cyan]1 - [yellow] Sort folders by type\n [bold red]X - [red]exit_program\n\n[white]\n --> ")
+    
+    if selected == "1":
+        sort()
+    
+    if selected == "x" or selected == "X":
+        clear()
+        exit_program()
+    
+    else:
+        menu()
+    
+ # initialize
+
+if __name__ == "__main__":
+
+    try:
+        if os.geteuid() != 0:
+            clear()
+            
+            print()
+            console.print(Panel("[yellow]You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. exit_programing."), style="red", justify="center")
+            print()
+        
+        else:
+             menu()
+    
+    except Exception as e:
+        print(e)
